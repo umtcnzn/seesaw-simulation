abbae8abce9c1fef40f45939fe2305c74a053d7f
umtcnzn <umtcn.znbk@gmail.com>
2026-03-15 01:14:32 +0300

    change the object size based on the weight, add random object colors and add drop sound effect. Also improve UI

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
+const BASE_WEIGHT = 20;
+const WEIGHT_MULTIPLIER = 5;
+const WEIGHT_COLORS = ['#10b981', '#3b82f6', '#f97316', '#8b5cf6', '#06b6d4', '#ec4899', '#eab308', '#ef4444'];
+
 let weights = [];
+
+const dropSound = new Audio('assets/object-drop-sound.mp3');
 
 const plank = document.querySelector('.plank');
 const resetBtn = document.querySelector('#reset-button');
+
 const leftWeightText = document.querySelector('#left-weight-text');
 const rightWeightText = document.querySelector('#right-weight-text');
 
 plank.addEventListener('click', (event) => {
 
     const clickedX = event.offsetX;
-
     const plankWidth = plank.offsetWidth;
-
     const centerX = plankWidth / 2;
-
     const distanceFromCenter = Math.abs(clickedX - centerX);
 
     const weight = randomWeight();
     const weightElement = createWeight(weight);
     weightElement.style.left = `${clickedX}px`;
     plank.appendChild(weightElement);
-    weights.push({ element: weightElement, distance: distanceFromCenter, weight, direction: clickedX < centerX ? 'left' : 'right' });
+    dropSound.play();
+    weights.push({ distance: distanceFromCenter, weight, direction: clickedX < centerX ? 'left' : 'right' });
     saveWeights();
     updateSeesaw();
 })
@@ -35,14 +40,19 @@ const createWeight = (weight) => {
     const weightElement = document.createElement('div');
     weightElement.classList.add('weight');
     weightElement.textContent = `${weight}kg`;
+    weightElement.style.width = `${BASE_WEIGHT + weight * WEIGHT_MULTIPLIER}px`;
+    weightElement.style.height = `${BASE_WEIGHT + weight * WEIGHT_MULTIPLIER}px`;
+    weightElement.style.backgroundColor = randomColor();
     return weightElement;
 }
 
+const randomColor = () => {
+    return WEIGHT_COLORS[Math.floor(Math.random() * WEIGHT_COLORS.length)];
+}
 
 const randomWeight = () => {
     return Math.floor(Math.random() * 10) + 1;
 }
-
 
 const updateSeesaw = () => {
     let totalLeftWeight = 0;
@@ -61,7 +71,6 @@ const updateSeesaw = () => {
     });
 
     const tiltAngle = Math.max(-30, Math.min(30, (totalRightTorque - totalLeftTorque) / 100));
-    console.log(`Left Torque: ${totalLeftTorque}, Right Torque: ${totalRightTorque}, Tilt Angle: ${tiltAngle}`);
     plank.style.transform = `rotate(${tiltAngle}deg)`;
     leftWeightText.textContent = totalLeftWeight;
     rightWeightText.textContent = totalRightWeight;
